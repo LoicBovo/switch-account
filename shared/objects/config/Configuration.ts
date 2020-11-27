19084828f1361fab39becb0a30f8806497760d5e
Loic <loic.bovo@dormakaba.com>
2020-11-27 14:12:32 -0500

    fix tab issue

--- shared/objects/config/Configuration.ts
+++ shared/objects/config/Configuration.ts
@@ -70,9 +70,9 @@ export class Configuration implements ConfigurationInt {
 		let config: string;
 
 		config = `[profile ${profileName}]
-	aws_access_key_id=${assumeRoleResponse.Credentials?.AccessKeyId}
-	aws_secret_access_key=${assumeRoleResponse.Credentials?.SecretAccessKey}
-	aws_session_token=${assumeRoleResponse.Credentials?.SessionToken}`;
+aws_access_key_id=${assumeRoleResponse.Credentials?.AccessKeyId}
+aws_secret_access_key=${assumeRoleResponse.Credentials?.SecretAccessKey}
+aws_session_token=${assumeRoleResponse.Credentials?.SessionToken}`;
 
 		console.info("overwritting files in config");
 		fs.writeFileSync(`${os.homedir()}/.aws/config`, config);
